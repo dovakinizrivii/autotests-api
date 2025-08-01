dcc626c477e42b21d7dfde50014dfa7d4827a842
dovakinizrivii <Sanjievandrei23@yandex.ru>
2025-08-01 15:10:56 +0300

    родительский апи клиент

---
+++ httpx_client.py
@@ -0,0 +1,21 @@
+import httpx
+
+auth_payload = {
+  "email": " ",
+  "password": "string"
+}
+
+auth_response = httpx.post('http://localhost:8000/api/v1/authentication/login', json=auth_payload)
+auth_response_data = auth_response.json()
+
+
+client = httpx.Client(
+    base_url="http://localhost:8000",
+    timeout=100,
+    headers={"Authorization": f"Bearer {auth_response_data['token']['accessToken']}"}
+)
+
+get_user_me_response = client.get('/api/v1/users/me')
+get_user_me_response_data = get_user_me_response.json()
+
+print('get user me data:', get_user_me_response_data)
